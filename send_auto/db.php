96c5ac1d30ed07467aa70b884097c25e17983a77
abiydoni <abiydoni@gmail.com>
2026-01-19 13:43:52 +0700

    Update db.php

--- send_auto/db.php
+++ send_auto/db.php
@@ -6,7 +6,7 @@ $user = 'appsbeem_admin';
 $pass = 'A7by777__';
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
+    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8mb4", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     
